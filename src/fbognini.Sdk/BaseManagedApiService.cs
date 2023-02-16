7c056ad9ab5ff6ae6a194f8e8c2cef5ce62f0bc9
Francesco Bognini <info@francescobognini.com>
2023-02-16 14:25:55 +0100

    logging for api result

---
+++ src/fbognini.Sdk/BaseManagedApiService.cs
@@ -0,0 +1,154 @@
+﻿using fbognini.Sdk.Extensions;
+using fbognini.Sdk.Interfaces;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+namespace fbognini.Sdk
+{
+    public abstract partial class BaseApiService
+    {
+        #region ApiResult
+
+        protected async Task<ApiResult> GetApiResult(string url)
+        {
+            return await ProcessApiResult(()
+                => client.GetAsync(url));
+        }
+
+        protected async Task<ApiResult<T>> GetApiResult<T>(string url)
+             where T : class
+        {
+            return await ProcessApiResult<T>(()
+                => client.GetAsync(url));
+        }
+
+
+        protected async Task<ApiResult> DeleteApiResult(string url)
+        {
+            return await ProcessApiResult(()
+                => client.DeleteAsync(url));
+        }
+
+        protected async Task<ApiResult<T>> DeleteApiResult<T>(string url)
+            where T : class
+        {
+            return await ProcessApiResult<T>(()
+                => client.DeleteAsync(url));
+        }
+
+        protected async Task<ApiResult> DeleteApiResult<TRequest>(string url, TRequest request)
+        {
+            return await ProcessApiResult(()
+                => client.DeleteAsJsonAsync(url, request));
+        }
+
+        protected async Task<ApiResult<T>> DeleteApiResult<T, TRequest>(string url, TRequest request)
+            where T : class
+        {
+            return await ProcessApiResult<T>(()
+                => client.DeleteAsJsonAsync(url, request));
+        }
+
+        protected async Task<ApiResult> PostApiResult(string url)
+        {
+            return await ProcessApiResult(()
+                => client.PostAsync(url, null));
+        }
+
+        protected async Task<ApiResult<T>> PostApiResult<T>(string url)
+           where T : class
+        {
+            return await ProcessApiResult<T>(()
+                => client.PostAsync(url, null));
+        }
+
+        protected async Task<ApiResult> PostApiResult(string url, HttpContent? content)
+        {
+            return await ProcessApiResult(()
+                => client.PostAsync(url, content));
+        }
+
+        protected async Task<ApiResult<T>> PostApiResult<T>(string url, HttpContent? content)
+            where T : class
+        {
+            return await ProcessApiResult<T>(()
+                => client.PostAsync(url, content));
+        }
+
+        protected async Task<ApiResult> PostApiResult<TRequest>(string url, TRequest request)
+        {
+            // client.PostAsJsonAsync don't use Header Content-type application/json
+            var content = new StringContent(JsonSerializer.Serialize(request, options), Encoding.UTF8, "application/json");
+            return await PostApiResult(url, content);
+        }
+
+        protected async Task<ApiResult<T>> PostApiResult<T, TRequest>(string url, TRequest request)
+            where T : class
+        {
+            // client.PostAsJsonAsync don't use Header Content-type application/json
+            var content = new StringContent(JsonSerializer.Serialize(request, options), Encoding.UTF8, "application/json");
+            return await PostApiResult<T>(url, content);
+        }
+
+        protected async Task<ApiResult<T>> PutApiResult<T, TRequest>(string url, TRequest request)
+           where T : class
+        {
+            return await ProcessApiResult<T>(()
+                => client.PutAsJsonAsync(url, request));
+        }
+
+        protected virtual async Task<ApiResult> ProcessApiResult(Func<Task<HttpResponseMessage>> action)
+        {
+            var response = await SendAction(action);
+            if (response.IsSuccessStatusCode)
+            {
+                return new ApiResult
+                {
+                    IsSuccess = true,
+                    StatusCode = response.StatusCode,
+                };
+            }
+
+            return new ApiResult
+            {
+                IsSuccess = false,
+                StatusCode = response.StatusCode,
+                Message = await response.Content.ReadAsStringAsync()
+            };
+        }
+
+        protected virtual async Task<ApiResult<T>> ProcessApiResult<T>(Func<Task<HttpResponseMessage>> action)
+           where T : class
+        {
+            var response = await SendAction(action);
+            if (response.IsSuccessStatusCode)
+            {
+                return new ApiResult<T>
+                {
+                    IsSuccess = true,
+                    StatusCode = response.StatusCode,
+                    Response = (await response.Content.ReadFromJsonAsync<T>(options))!
+                };
+            }
+
+            return new ApiResult<T>
+            {
+                IsSuccess = false,
+                StatusCode = response.StatusCode,
+                Message = await response.Content.ReadAsStringAsync()
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ src/fbognini.Sdk/BasePrivateApiService.cs
@@ -0,0 +1,136 @@
+﻿using fbognini.Sdk.Extensions;
+using fbognini.Sdk.Interfaces;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+namespace fbognini.Sdk
+{
+    public abstract partial class BaseApiService
+    {
+        private async Task<HttpResponseMessage> SendAction(Func<Task<HttpResponseMessage>> action)
+        {
+            var baseAddress = client.BaseAddress?.ToString() ?? string.Empty;
+            var sdk = this.GetType().Namespace!;
+
+            var propertys = new Dictionary<string, object>()
+            {
+                ["IsSdk"] = true,
+                ["Sdk"] = sdk,
+                ["BaseAddress"] = baseAddress
+            };
+
+            var index = action.Method.Name.IndexOf("ApiResult>");
+            if (index == -1)
+            {
+                index = action.Method.Name.IndexOf("Api>");
+            }
+
+            string method = action.Method.Name[1..index].ToUpper();
+            propertys.Add("Method", method);
+
+            var type = action.Target!.GetType();
+
+            var url = type.GetField("url")!.GetValue(action.Target)!.ToString()!;
+            var uri = $"{baseAddress}{url}";
+            propertys.Add("RequestUrl", url);
+            propertys.Add("Uri", uri);
+            var contentField = type.GetField("content");
+            if (contentField != null)
+            {
+                var content = contentField.GetValue(action.Target)!;
+                if (content is JsonContent json)
+                {
+                    propertys.Add("Request", JsonSerializer.Serialize(json.Value, options));
+                }
+                else if (content is ByteArrayContent byteArray)
+                {
+                    propertys.Add("Request", await byteArray.ReadAsStringAsync());
+                }
+            }
+
+            try
+            {
+                using (logger.BeginScope(propertys))
+                {
+                    logger.LogInformation("{Sdk} requesting {Method} {Uri}", sdk, method, uri);
+                }
+
+                HttpResponseMessage message;
+                if (currentUserService != null)
+                {
+                    if (await currentUserService.IsAuthenticated())
+                    {
+                        await SetAccessToken();
+                    }
+
+                    message = await AuthenticationEnsuringPolicy.ExecuteAsync(() => ExecuteAction(action));
+                }
+                else
+                {
+                    message = await ExecuteAction(action);
+                }
+
+                int statusCode = (int)message.StatusCode;
+
+                var headers = GetResponseHeaders(message.Headers).ToList();
+                propertys.Add("ResponseHeaders", headers);
+
+                propertys.Add("IsSuccessStatusCode", message.IsSuccessStatusCode);
+                propertys.Add("StatusCode", statusCode);
+
+                var rawResponse = await message.Content.ReadAsStringAsync();
+                propertys.Add("RawResponse", rawResponse);
+
+                if (httpErrorHandler != null)
+                {
+                    await httpErrorHandler.HandleResponse(message);
+                }
+
+                using (logger.BeginScope(propertys))
+                {
+                    logger.LogInformation("{Sdk} {Method} {Uri} responded {StatusCode}", sdk, method, uri, statusCode);
+                }
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                using (logger.BeginScope(propertys))
+                {
+                    logger.LogError(ex, "{Sdk} failed to ask for {Method} {Uri}", sdk, method, uri);
+                }
+
+                throw;
+            }
+
+            IEnumerable<KeyValuePair<string, string>> GetResponseHeaders(HttpResponseHeaders httpResponseHeaders)
+            {
+                var baseHeaders = httpResponseHeaders.GetType().GetProperties().Select(x => x.Name!).ToList();
+                baseHeaders.Add("Set-Cookie");
+                baseHeaders.Add("Cache-Control");
+                baseHeaders.Add("X-AspNet-Version");
+                baseHeaders.Add("X-Powered-By");
+                baseHeaders.Add("Strict-Transport-Security");
+                baseHeaders.Add("X-Content-Type-Options");
+                baseHeaders.Add("X-Frame-Options");
+                baseHeaders.Add("X-XSS-Protection");
+                baseHeaders.Add("Transfer-Encoding");
+                var headers = httpResponseHeaders
+                    .Where(x => baseHeaders.Contains(x.Key) == false && x.Key.StartsWith("Access-Control") == false).ToList();
+
+                return headers.SelectMany(x => x.Value, (header, value) => new KeyValuePair<string, string>(header.Key, value));
+            }
+        }
+
+    }
+}
